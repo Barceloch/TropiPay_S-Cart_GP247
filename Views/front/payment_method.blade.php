80181168240fc59e8df4df722da0e8100ca7759c
BarceloCh <barceloch@gmail.com>
2025-11-15 08:15:01 -0500

    Delete Views directory

--- Views/front/payment_method.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group">
-    <label>
-        <input type="radio" name="payment_method" value="tropipay" checked>
-        TropiPay (tarjeta / saldo TropiPay)
-    </label>
-    <p class="small text-muted">
-        Serás redirigido a TropiPay para completar el pago de forma segura.
-    </p>
-</div>
--- Views/front/return.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('gp247-front::layouts.shop')
-
-@section('content')
-<div class="container my-5">
-    <h2>Resultado del pago con TropiPay</h2>
-    <p>{{ $message }}</p>
-    <a href="{{ url('/') }}" class="btn btn-primary">Volver a la tienda</a>
-</div>
-@endsection
