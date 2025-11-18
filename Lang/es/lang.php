90c4594bdb27956fedaf0ca0b66825856a1e8e2b
BarceloCh <barceloch@gmail.com>
2025-11-18 04:46:02 -0500

    Delete Lang directory

--- Lang/es/lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'title' => 'TropiPay',
-    'description' => 'Paga de forma segura con TropiPay - Billetera electrónica',
-    'payment_info' => 'Serás redirigido a TropiPay para completar el pago de forma segura.',
-];
